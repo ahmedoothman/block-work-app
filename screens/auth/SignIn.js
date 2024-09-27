fed350cb4568b8a89f14dbecb66a319cee1f553f
ahmedoothman <ahmed.othman.ghazzawi@gmail.com>
2024-09-27 21:57:12 +0300

    upload new

--- screens/auth/SignIn.js
+++ screens/auth/SignIn.js
@@ -7,40 +7,46 @@ import signInStyle from '../../styles/Screens/SignIn_Style';
 import InputField from '../../components/inputs/auth/InputField';
 import AppButton from '../../components/btns/AppButton';
 import { useNavigation } from '@react-navigation/native';
-
+import { loginService } from '../../services/authService';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../../store/authSlice';
 const SignIn = () => {
   const navigation = useNavigation();
-
+  const dispatch = useDispatch();
   //'  Main States
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
   //' SignIn Function
-  function handleSignIn() {
-    console.log("email ", email, "password ", password);
-
-
-    clearInputs()
-  }
+  const handleSignIn = async () => {
+    setLoading(true);
+    const response = await loginService({ email, password });
+    if (response.status === 'success') {
+      // response.data contains the user data , set store
+      dispatch(authActions.login(response.data));
+      // navigate to freelancer base
+    } else {
+      // show error message
+      setError(true);
+      setErrorMessage(response.message);
+    }
+    setLoading(false);
+    clearInputs();
+  };
 
   //' clear InputFields
   function clearInputs() {
-    setEmail("")
-    setPassword("")
+    setEmail('');
+    setPassword('');
   }
-
-
 
   return (
     <SafeAreaView
-      style={[
-        signInStyle.container, { backgroundColor: "#121114" }
-      ]}
+      style={[signInStyle.container, { backgroundColor: '#121114' }]}
     >
-
-      <View style={[
-        signInStyle.content,
-      ]}>
+      <View style={[signInStyle.content]}>
         {/* //' Logo Container */}
         <Logo />
 
@@ -48,29 +54,25 @@ const SignIn = () => {
         <InputField
           onChange={(value) => setEmail(value)}
           value={email}
-          placeholder="Email"
+          placeholder='Email'
         />
         {/* //' Password */}
         <InputField
           onChange={(value) => setPassword(value)}
           value={password}
-          placeholder="Password"
+          placeholder='Password'
           isPassword={true}
         />
 
-
         {/* //' Login Btn */}
-        <AppButton onPress={() => handleSignIn()} buttonTitle={"login"} />
+        <AppButton onPress={() => handleSignIn()} buttonTitle={'login'} />
 
         <TouchableOpacity
           onPress={() => navigation.navigate('ForgotPassword')} // Navigate to SignUp component
         >
-          <Text style={signInStyle.forget}>
-            Forget Password ?
-          </Text>
+          <Text style={signInStyle.forget}>Forget Password ?</Text>
         </TouchableOpacity>
       </View>
-
     </SafeAreaView>
   );
 };
