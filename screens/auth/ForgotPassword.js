08a93fa0f1e348e58c85bcf8c7fc7c930086be27
ahmedoothman <ahmed.othman.ghazzawi@gmail.com>
2024-09-29 23:30:24 +0300

    updated

--- screens/auth/ForgotPassword.js
+++ screens/auth/ForgotPassword.js
@@ -1,24 +1,23 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import InputField from "../../components/inputs/auth/InputField";
-import { useNavigation } from "@react-navigation/native";
-import Logo from "../../components/Public/logo";
-import AppButton from "../../components/btns/AppButton";
-import theme from "../../theme";
-import { Snackbar } from "react-native-paper";
-import CustomeSnackBar from "../../components/Public/CustomeSnackBar";
-import { forgotPasswordService } from "../../services/userService";
-import { useDispatch } from "react-redux";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import InputField from '../../components/inputs/auth/InputField';
+import { useNavigation } from '@react-navigation/native';
+import Logo from '../../components/Public/logo';
+import AppButton from '../../components/btns/AppButton';
+import theme from '../../theme';
+import { Snackbar } from 'react-native-paper';
+import CustomeSnackBar from '../../components/Public/CustomeSnackBar';
+import { forgotPasswordService } from '../../services/userService';
 
 const ForgotPassword = () => {
   const navigation = useNavigation();
 
   //'  Main States
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
 
   const [alert, setAlert] = useState(false);
-  const [alertMessage, setAlertMessage] = useState("");
+  const [alertMessage, setAlertMessage] = useState('');
   const [isSuccess, setIsSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -33,17 +32,17 @@ const ForgotPassword = () => {
     }
     const response = await forgotPasswordService(email);
     setAlert(true);
-    if (response.status == "success") {
+    if (response.status == 'success') {
       setIsSuccess(true);
       setAlertMessage(response.data);
-      console.log("success response", isSuccess, alert, email, response.data);
+      console.log('success response', isSuccess, alert, email, response.data);
       setTimeout(() => {
-        navigation.navigate("ResetPassword");
+        navigation.navigate('ResetPassword');
       }, 2000);
     } else {
       setIsSuccess(false);
       setAlertMessage(response.message);
-      console.log("error response", isSuccess, alert, email, response.message);
+      console.log('error response', isSuccess, alert, email, response.message);
     }
     setLoading(false);
     clearInputs();
@@ -51,18 +50,18 @@ const ForgotPassword = () => {
 
   //' clear InputFields
   function clearInputs() {
-    setEmail("");
+    setEmail('');
   }
 
   //' Validate Inputs
   const validateInputs = () => {
     setAlert(false);
-    setAlertMessage("");
+    setAlertMessage('');
     // ------ Email validation
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!email || !emailPattern.test(email)) {
       setAlert(true);
-      setAlertMessage("Please enter a valid email address.");
+      setAlertMessage('Please enter a valid email address.');
       setLoading(false);
       return false;
     }
@@ -73,7 +72,8 @@ const ForgotPassword = () => {
       style={[
         styles.container,
         { backgroundColor: theme.colors.secondaryDark },
-      ]}>
+      ]}
+    >
       <View style={[styles.content]}>
         {/* //' Logo Container */}
         <Logo />
@@ -82,13 +82,13 @@ const ForgotPassword = () => {
         <InputField
           onChange={(value) => setEmail(value)}
           value={email}
-          placeholder="Email"
+          placeholder='Email'
         />
 
         {/* //' Login Btn */}
         <AppButton
           onPress={() => handleSubmit()}
-          buttonTitle={"Submit"}
+          buttonTitle={'Submit'}
           loading={loading}
         />
       </View>
@@ -97,10 +97,10 @@ const ForgotPassword = () => {
         visible={alert}
         alertMessage={alertMessage}
         onDismissSnackBar={onDismissSnackBar}
-        undoText="Undo"
-        undoColor="black"
-        bgColor={isSuccess ? theme.colors.colorTextBlue : "red"}
-        messageColor="#fff"
+        undoText='Undo'
+        undoColor='black'
+        bgColor={isSuccess ? theme.colors.colorTextBlue : 'red'}
+        messageColor='#fff'
       />
     </SafeAreaView>
   );
@@ -109,16 +109,16 @@ const ForgotPassword = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingHorizontal: 20,
   },
   content: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    height: "50%",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '50%',
   },
 });
 
