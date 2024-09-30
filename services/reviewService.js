af817f6f92b16ad1b8f8cfc59896aacba42225d4
ahmedoothman <ahmed.othman.ghazzawi@gmail.com>
2024-09-30 23:14:33 +0300

    added new services

---
+++ services/reviewService.js
@@ -0,0 +1,58 @@
+import { API_URL } from '../constants/global/api';
+import { getToken } from '../storage/tokenStorage';
+import axios from 'axios';
+
+const PATH = 'api/reviews';
+
+//noran
+export const getReviewsService = async (userId) => {
+  let token = await getToken();
+  try {
+    const response = await axios.get(`${API_URL}/${PATH}/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return { status: 'success', data: response.data.data };
+  } catch (error) {
+    if (error.code === 'ERR_NETWORK') {
+      return {
+        status: 'error',
+        statusCode: error.code,
+        message: error.message + ' Please check your internet connection',
+      };
+    } else {
+      return {
+        status: 'error',
+        statusCode: error.response.statusCode,
+        message: error.response.data.message,
+      };
+    }
+  }
+};
+// not now
+export const addReviewService = async (review) => {
+  let token = await getToken();
+  try {
+    const response = await axios.post(`${API_URL}/${PATH}`, review, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return { status: 'success', data: response.data.data };
+  } catch (error) {
+    if (error.code === 'ERR_NETWORK') {
+      return {
+        status: 'error',
+        statusCode: error.code,
+        message: error.message + ' Please check your internet connection',
+      };
+    } else {
+      return {
+        status: 'error',
+        statusCode: error.response.statusCode,
+        message: error.response.data.message,
+      };
+    }
+  }
+};
